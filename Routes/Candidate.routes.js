9a08da27bdf87eb4641bc790cd4fdd0898000084
Mugisha-isaac <mugishaisaac2001@gmail.com>
2022-07-03 22:42:15 +0200

    [feat] documenting apis

--- Routes/Candidate.routes.js
+++ Routes/Candidate.routes.js
@@ -3,6 +3,87 @@ const {create,getAll} = require("../controllers/Candidate.controller");
 
 const router = express.Router();
 
+
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *    Candidate:
+ *       type: object
+ *       required:
+ *         -names
+ *         -votes
+ *         -imgUrl
+ *       properties:
+ *         id:
+ *           type: String
+ *           description: auto generated id by mongodb.
+ *         names:
+ *           type: String
+ *           description: candidate both names
+ *           required: true
+ *         votes:
+ *           type: Number
+ *           description: candidate votes. by default it is set to 1.
+ *           required: true
+ *         imgUrl:
+ *           type: String
+ *           description: candidate image url
+ *           required: true  
+ *            
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Candidate
+ *   description: candidate managing apis
+ */
+
+
+/**
+ * @swagger
+ * /candidates:
+ *   get:
+ *     summary: finds all candidates
+ *     tags: [Candidates]
+ *     responses:
+ *       200:
+ *         description: list of candidates
+ *         content:
+ *           application/json:
+ *             schema: 
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Candidate'
+ */
+
+
+
+/**
+ * @swagger
+ * /candidates/post:
+ *   post:
+ *     summary: Create a new candidate. 
+ *     tags: [Candidates]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Candidate'
+ *     responses:
+ *       200:
+ *         description:   candidate  successfully created
+ *         content:
+ *             application/json:
+ *               schema:
+ *                 $ref: '#/components/schemas/Candidate'
+ *       500:
+ *         description:  Server error
+ */
+
 router.route('/post').post(create);
 router.route('/').get(getAll);
 
--- Routes/User.routes.js
+++ Routes/User.routes.js
@@ -4,6 +4,98 @@ const {findAll,getById} = require('../controllers/User.controller');
 
 const router = express.Router();
 
+
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *    User:
+ *       type: object
+ *       required:
+ *         -nationalId
+ *         -email
+ *         -names
+ *         -password  
+ *       properties:
+ *         id:
+ *           type: String
+ *           description: auto generated id by mongodb.
+ *         names:
+ *           type: String
+ *           description: user both names
+ *           required: true
+ *         email:
+ *           type: string
+ *           description: a unique user email address
+ *           required: true
+ *           unique: true
+ *         nationalId:
+ *           type: Number
+ *           description: user allowed National ID number. it is 16 digits long no short no long
+ *           required: true
+ *         password:
+ *           type: String
+ *           description: This the password for user to login. it is hashed. You need to validate if it contains,characters,numbers, symbols... it should at most be 255 and 8 characters at least
+ *           required: true   
+ *         voted:
+ *           type: Boolean
+ *           required: true            
+ *           description: by default voted status is set to false
+ *            
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: user managing apis
+ */
+
+
+/**
+ * @swagger
+ * /user:
+ *   get:
+ *     summary: finds all users
+ *     tags: [Users]
+ *     responses:
+ *       200:
+ *         description: list of users
+ *         content:
+ *           application/json:
+ *             schema: 
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
+
+
+
+/**
+ * @swagger
+ * /user/{id}:
+ *   get:
+ *     summary: get a user by id
+ *     tags: [User]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: String
+ *         required: true
+ *         description: The id of a user you want to get
+ *     responses:
+ *       200:
+ *         description: User description by id
+ *         contents:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ *       400:
+ *         description: user not found 
+ */
+
 router.route('/').get(findAll);
 router.route('/:id').get(getById);
 
